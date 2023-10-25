b051a69f70ff938f840cd867220fe862d709a8bf
PedroF <pedro.mferreira@outlook.pt>
2023-10-25 22:52:12 +0100

    Project upload

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,37 @@
+import { Component, Output, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styles: [
+  ]
+})
+export class FiltersComponent implements OnInit, OnDestroy{
+  @Output() showCategory = new EventEmitter<string>();
+
+  categoriesSubscription: Subscription | undefined;
+  categories : Array<string> | undefined;
+
+  constructor(private storeService: StoreService){
+    
+  }
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService.getAllCategories()
+    .subscribe( (response) => {
+      this.categories = response;
+    });
+  }
+
+  ngOnDestroy(): void {
+      if(this.categoriesSubscription){
+        this.categoriesSubscription.unsubscribe();
+      }
+  }
+  
+  onShowCategory(category: string):void{
+    this.showCategory.emit(category)
+  }
+}
